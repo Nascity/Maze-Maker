c5b5abfceaf36e184d23515ffdef3966680a570c
ssr <victory8500@naver.com>
2022-10-25 14:34:37 +0900

    Uploaded an example

---
+++ examples/mazemaker/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using MazeMaker;
+using static System.Console;
+
+namespace Showcase
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				WriteLine(new Maze(int.Parse(args[0]), int.Parse(args[1])).Generate());
+			}
+			catch (Exception e)
+			{
+				WriteLine(e.Message);
+			}
+		}
+	}
+}
